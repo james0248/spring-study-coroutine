502eb911e5e36ab10af734c8a0ee06ec8a5caa40
Hyeonseok Jung <hsjames0248@gmail.com>
2023-04-03 01:16:36 +0900

    Initial commit

---
+++ src/main/kotlin/com/scatterlab/springstudy/SpringstudyApplication.kt
@@ -0,0 +1,11 @@
+package com.scatterlab.springstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringstudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringstudyApplication>(*args)
+}
