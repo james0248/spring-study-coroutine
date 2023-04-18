894a045b6a765a2a4b33160b5ae4917ad3d9133b
Hyeonseok Jung <hsjames0248@gmail.com>
2023-04-19 00:06:28 +0900

    fix: Change return type from Flow to List

--- src/main/kotlin/com/scatterlab/springstudy/dao/ItemRepository.kt
+++ src/main/kotlin/com/scatterlab/springstudy/dao/ItemRepository.kt
@@ -1,18 +1,17 @@
 package com.scatterlab.springstudy.dao
 
 import com.scatterlab.springstudy.model.Item
-import kotlinx.coroutines.flow.Flow
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import java.util.UUID
 
 interface ItemRepository : CoroutineCrudRepository<Item, UUID> {
     suspend fun save(item: Item): Item
-    suspend fun findByName(name: String): Flow<Item>
-    suspend fun findByNameOrDetail(name: String, detail: String): Flow<Item>
-    suspend fun findByPriceLessThan(price: Int): Flow<Item>
-    suspend fun findByPriceLessThanOrderByPriceDesc(price: Int): Flow<Item>
+    suspend fun findByName(name: String): List<Item>
+    suspend fun findByNameOrDetail(name: String, detail: String): List<Item>
+    suspend fun findByPriceLessThan(price: Int): List<Item>
+    suspend fun findByPriceLessThanOrderByPriceDesc(price: Int): List<Item>
 
     @Query("SELECT * FROM items WHERE detail LIKE :detail")
-    suspend fun findByDetailLike(detail: String): Flow<Item>
+    suspend fun findByDetailLike(detail: String): List<Item>
 }
